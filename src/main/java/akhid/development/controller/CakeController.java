ece6c7fb35cbf80cd0bdf20f5d08f7028e346c04
akhid.nukhlis <akhid.nukhlis@privyid.tech>
2023-03-02 13:18:27 +0700

    add func findAll, findById, updateById, deleteById

--- src/main/java/akhid/development/controller/CakeController.java
+++ src/main/java/akhid/development/controller/CakeController.java
@@ -2,11 +2,13 @@ package akhid.development.controller;
 
 import javax.inject.Inject;
 import javax.validation.ValidationException;
+import javax.validation.constraints.Pattern;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import akhid.development.dto.CakeRequestDto;
+import akhid.development.model.Cake;
 import akhid.development.service.CakeService;
 
 import java.util.HashMap;
@@ -21,7 +23,7 @@ public class CakeController {
 
     @Path("/submit")
     @POST
-    public Response submit(CakeRequestDto dto) {
+    public Response submit (CakeRequestDto dto) {
         Map<String, Object> result = new HashMap<>();
 
         try {
@@ -32,12 +34,6 @@ public class CakeController {
             return Response.status(Response.Status.CREATED)
                     .entity(result)
                     .build();
-        } catch (ValidationException ex) {
-            result.put("statusCode", 300);
-            result.put("message", ex.getMessage());
-            return Response.ok()
-                    .entity(result)
-                    .build();
         } catch (Exception ex) {
             ex.printStackTrace();
             result.put("statusCode", 500);
@@ -46,6 +42,104 @@ public class CakeController {
                     .entity(result)
                     .build();
         }
+    }
 
+    @GET
+    public Response findAll () {
+        Map<String, Object> result = new HashMap<>();
+
+        try {
+            result.put("statusCode", 200);
+            result.put("data", Cake.findAll().list());
+            return Response.ok(result).build();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            result.put("statusCode", 500);
+            result.put("message", "INTERNAL_SERVER_ERROR");
+            return Response.serverError()
+                    .entity(result)
+                    .build();
+        }
+    }
+
+    @Path("/id/{id}")
+    @GET
+    public Response findById (
+            @PathParam("id")
+            @Pattern(regexp = "[0-9a-zA-Z]{32,32}", message = "Pattern not match")
+            String uuid) {
+        Map<String, Object> result = new HashMap<>();
+
+        try {
+            result.put("statusCode", 200);
+            result.put("data", cakeService.findById(uuid));
+            return Response.ok(result).build();
+        } catch (ValidationException ex) {
+            result.put("message", ex.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(result).build();
+        }  catch (Exception ex) {
+            ex.printStackTrace();
+            result.put("statusCode", 500);
+            result.put("message", "INTERNAL_SERVER_ERROR");
+            return Response.serverError()
+                    .entity(result)
+                    .build();
+        }
+    }
+
+    @Path("/update/id/{id}")
+    @PUT
+    public Response updateById (
+            @PathParam("id")
+            @Pattern(regexp = "[0-9a-zA-Z]{32,32}", message = "Pattern not match")
+            String uuid, CakeRequestDto dto) {
+        Map<String, Object> result = new HashMap<>();
+
+        try {
+            result.put("statusCode", 200);
+            result.put("data", cakeService.updateById(uuid, dto));
+            return Response.ok(result).build();
+        } catch (ValidationException ex) {
+            result.put("message", ex.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(result).build();
+        } catch (NotFoundException nfe) {
+            result.put("message", nfe.getMessage());
+            return Response.status(Response.Status.NOT_FOUND).entity(result).build();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            result.put("statusCode", 500);
+            result.put("message", "INTERNAL_SERVER_ERROR");
+            return Response.serverError()
+                    .entity(result)
+                    .build();
+        }
+    }
+
+    @Path("/delete/id/{id}")
+    @DELETE
+    public Response deleteById (
+            @PathParam("id")
+            @Pattern(regexp = "[0-9a-zA-Z]{32,32}", message = "Pattern not match")
+            String uuid) {
+            Map<String, Object> result = new HashMap<>();
+
+        try {
+            result.put("statusCode", 200);
+            result.put("data", cakeService.deleteById(uuid));
+            return Response.ok(result).build();
+        } catch (ValidationException ex) {
+            result.put("message", ex.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(result).build();
+        } catch (NotFoundException nfe) {
+            result.put("message", nfe.getMessage());
+            return Response.status(Response.Status.NOT_FOUND).entity(result).build();
+        }  catch (Exception ex) {
+            ex.printStackTrace();
+            result.put("statusCode", 500);
+            result.put("message", "INTERNAL_SERVER_ERROR");
+            return Response.serverError()
+                    .entity(result)
+                    .build();
+        }
     }
 }
--- src/main/java/akhid/development/service/CakeService.java
+++ src/main/java/akhid/development/service/CakeService.java
@@ -2,22 +2,21 @@ package akhid.development.service;
 
 import akhid.development.dto.CakeRequestDto;
 import akhid.development.model.Cake;
+import akhid.development.util.BasicUtil;
+import com.google.common.base.Strings;
 import com.oracle.svm.core.annotate.Inject;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
-import java.text.ParseException;
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import javax.validation.ValidationException;
+import javax.ws.rs.NotFoundException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @ApplicationScoped
 public class CakeService {
@@ -28,12 +27,13 @@ public class CakeService {
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
 
     public Map<String, Object> submit(CakeRequestDto dto) {
-        
-        String uuid = UUID.randomUUID().toString().replace("-", "");
+
+        String uuid = UUID.randomUUID().toString();
         Timestamp ldt = new Timestamp(new Date().getTime());
+        Boolean active = true;
 
         // store to database
-        insertData(dto, uuid, ldt);
+        insertData(dto, uuid, active, ldt);
 
         Map<String, Object> result = new HashMap<>();
         result.put("id", uuid);
@@ -43,20 +43,121 @@ public class CakeService {
         result.put("created_at", sdf.format(ldt));
 
         return result;
+
+    }
+
+    public Map<String, Object> findById(String id) throws Exception {
+
+        if (Strings.isNullOrEmpty(id)) {
+            throw new ValidationException("BAD_REQUEST");
+        }
+
+        Query q = em.createNativeQuery(
+                 "SELECT " +
+                    "    c.id AS id,\n" +
+                    "    c.tittle AS tittle,\n" +
+                    "    c.description AS description,\n" +
+                    "    c.rating AS rating,\n" +
+                    "    c.image AS image,\n" +
+                    "    c.active AS active,\n" +
+                    "    c.createdAt AS create_at\n" +
+                    "FROM cake c\n" +
+                    "WHERE c.id = :UUID"
+        );
+        q.setParameter("UUID", id);
+        List result = BasicUtil.createListOfMapFromArray(
+                q.getResultList(),
+                "id", "tittle", "description", "rating", "image", "active", "create_at"
+        );
+
+        return result.size() > 0 ? (Map<String, Object>) result.get(0) : null;
+
+    }
+
+    public Map<String, Object> updateById(String id, CakeRequestDto dto) {
+        Timestamp ldt = new Timestamp(new Date().getTime());
+
+        if (Strings.isNullOrEmpty(id)) {
+            throw new ValidationException("BAD_REQUEST");
+        }
+
+        Cake cake = Cake.findById(id);
+        if(cake == null) {
+            throw new NotFoundException("NOT_FOUND");
+        }
+
+        // store to database
+        updateData(id, dto, ldt);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("id", id);
+        result.put("tittle", dto.tittle);
+        result.put("description", dto.description);
+        result.put("image", dto.image);
+        result.put("updated_at", sdf.format(ldt));
+
+        return result;
+    }
+
+    public Map<String, Object> deleteById(String id) {
+        Timestamp ldt = new Timestamp(new Date().getTime());
+        Boolean active = false;
+
+        if (Strings.isNullOrEmpty(id)) {
+            throw new ValidationException("BAD_REQUEST");
+        }
+
+        Cake cake = Cake.findById(id);
+        if(cake == null) {
+            throw new NotFoundException("NOT_FOUND");
+        }
+
+        // store to database
+        deleteData(id, active, ldt);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("id", id);
+        result.put("deleted_at", sdf.format(ldt));
+
+        return result;
     }
 
     @Transactional
-    public void insertData(CakeRequestDto dto, String uuid, Timestamp ldt) {
+    public void insertData (CakeRequestDto dto, String id, Boolean active, Timestamp ldt) {
 
         Cake cake = new Cake();
-        cake.id = uuid;
+        cake.id = id;
         cake.tittle = dto.tittle;
         cake.description = dto.description;
         cake.rating = dto.rating;
         cake.image = dto.image;
-        cake.active = true;
-        cake.createdAt =  ldt;
+        cake.active = active;
+        cake.createdAt = ldt;
         cake.persist();
 
     }
+
+    @Transactional
+    public void updateData (String id, CakeRequestDto dto, Timestamp ldt) {
+
+        Cake cake = Cake.findById(id);
+        cake.tittle = dto.tittle;
+        cake.description = dto.description;
+        cake.rating = dto.rating;
+        cake.image = dto.image;
+        cake.updatedAt =  ldt;
+        cake.persist();
+
+    }
+
+    @Transactional
+    public void deleteData (String id, Boolean active, Timestamp ldt) {
+
+        Cake cake = Cake.findById(id);
+        cake.active = active;
+        cake.deletedAt =  ldt;
+        cake.persist();
+
+    }
+
 }
